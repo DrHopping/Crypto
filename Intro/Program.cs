f098492df870d73d5eade4b4c3f877c894c977ce
Mykyta Pedorenko <mykyta_pedorenko@epam.com>
2021-10-15 17:37:22 +0300

    Decode intro

--- Intro/Program.cs
+++ Intro/Program.cs
@@ -1,4 +1,8 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
 
 namespace Intro
 {
@@ -6,7 +10,24 @@ namespace Intro
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            var cipherBinary = File.ReadAllText(@"..\..\..\Files\Ciphertext.txt");
+            var cipherBytes = GetBytesFromBinaryString(cipherBinary);
+            var cipherText = Encoding.ASCII.GetString(cipherBytes);
+            var decodedText = Encoding.UTF8.GetString(Convert.FromBase64String(cipherText));
+            File.WriteAllText(@"..\..\..\Files\decoded.txt", decodedText);
+        }
+
+        private static byte[] GetBytesFromBinaryString(string binary)
+        {
+            var list = new List<byte>();
+
+            for (var i = 0; i < binary.Length; i += 8)
+            {
+                var t = binary.Substring(i, 8);
+                list.Add(Convert.ToByte(t, 2));
+            }
+
+            return list.ToArray();
         }
     }
 }
