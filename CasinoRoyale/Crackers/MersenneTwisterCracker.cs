e72a4debee679611f5fcdf55cddd92dfe9bd101a
Mykyta Pedorenko <mykyta_pedorenko@epam.com>
2021-11-29 23:12:16 +0200

    Add MersenneTwisterCracker

---
+++ CasinoRoyale/Crackers/MersenneTwisterCracker.cs
@@ -0,0 +1,22 @@
+﻿using CasinoRoyale.RandomGenerators;
+
+namespace CasinoRoyale.Crackers
+{
+    public class MersenneTwisterCracker
+    {
+        private readonly ulong _seed;
+        private readonly MersenneTwister _generator;
+
+        public MersenneTwisterCracker(ulong seed)
+        {
+            _seed = seed;
+            _generator = new MersenneTwister();
+            _generator.init_genrand(seed);
+        }
+
+        public ulong Next()
+        {
+            return _generator.genrand_uint32();
+        }
+    }
+}
--- CasinoRoyale/Program.cs
+++ CasinoRoyale/Program.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Threading.Tasks;
 using CasinoRoyale.Crackers;
 using CasinoRoyale.Enums;
+using CasinoRoyale.Models;
 using CasinoRoyale.RandomGenerators;
 using CasinoRoyale.Services;
 
@@ -14,7 +15,8 @@ namespace CasinoRoyale
         static async Task Main(string[] args)
         {
             var casino = await Casino.CreateCasino();
-            await WinLcg(casino);
+            //await WinLcg(casino);
+            await WinMt(casino);
         }
 
         private static async Task WinLcg(Casino casino)
@@ -27,7 +29,25 @@ namespace CasinoRoyale
             };
 
             var cracker = new LcgCracker(randomNumbers);
-            Console.WriteLine((await casino.Play(GameMode.Lcg, 500, (int) cracker.Next())).Message);
+            
+            Result result;
+            do
+            {
+                result = await casino.Play(GameMode.Lcg, 500, (int)cracker.Next());
+            } while (result.Account.Money < 1000000);
+            Console.WriteLine(result.Message);
+        }
+
+        private static async Task WinMt(Casino casino)
+        {
+            var seed = DateTimeOffset.UtcNow.ToUnixTimeSeconds();
+            var mtCracker = new MersenneTwisterCracker((ulong)seed);
+            Result result;
+            do
+            {
+                result = await casino.Play(GameMode.Mt, 500, (int)mtCracker.Next());
+            } while (result.Account.Money < 1000000);
+            Console.WriteLine(result.Message);
         }
     }
 }
