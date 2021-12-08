d8a514c5858bc289248a9fe0226ef84182edae70
Mykyta Pedorenko <mpedorenko@corelogic.com>
2021-12-08 21:32:47 +0200

    Switch library for Argon2 to Sodium.Core

--- PasswordGenerator/Helpers/Hasher.cs
+++ PasswordGenerator/Helpers/Hasher.cs
@@ -1,25 +1,17 @@
 ﻿using System;
 using System.Security.Cryptography;
 using System.Text;
-using NSec.Cryptography;
-using HashAlgorithm = NSec.Cryptography.HashAlgorithm;
+using Sodium;
 
 namespace PasswordGenerator.Helpers
 {
     public class Hasher
     {
         private MD5 _md5;
-        private Argon2id _argon;
         
         public Hasher()
         {
             _md5 = MD5.Create();
-            _argon = new Argon2id(new Argon2Parameters()
-            {
-                DegreeOfParallelism = 1,
-                MemorySize = 1024,
-                NumberOfPasses = 1
-            });
         }
 
         public string ComputeMd5Hash(string input)
@@ -28,20 +20,6 @@ namespace PasswordGenerator.Helpers
             return Convert.ToHexString(hash);
         }
 
-        public (string Hash, string Salt) ComputeArgon2IdHash(string input)
-        {
-            var passwordBytes = Encoding.UTF8.GetBytes(input);
-            var salt = CreateSalt();
-            var hash = _argon.DeriveBytes(passwordBytes, salt, 16);
-            return (Convert.ToHexString(hash), Convert.ToHexString(salt));
-        }
-
-        private byte[] CreateSalt()
-        {
-            var buffer = new byte[16];
-            var rng = new RNGCryptoServiceProvider();
-            rng.GetBytes(buffer);
-            return buffer;
-        }
+        public string ComputeArgon2IdHash(string input) => PasswordHash.ArgonHashString(input).TrimEnd('\0');
     }
 }
--- PasswordGenerator/Program.cs
+++ PasswordGenerator/Program.cs
@@ -1,4 +1,6 @@
-﻿using System.Globalization;
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
 using System.IO;
 using System.Linq;
 using System.Security.Cryptography;
@@ -10,33 +12,26 @@ namespace PasswordGenerator
 {
     class Program
     {
+
+        private static int _passwordsCount = 100000;
         static void Main(string[] args)
         {
             var hasher = new Hasher();
             var generator = new Helpers.PasswordGenerator(0.05, 0.05, 0.10, 0.8);
-            SaveArgonPasswords(hasher, generator, 100000);
-            SaveMd5(hasher, generator, 100000);
+            var passwords = generator.GeneratePasswords(_passwordsCount);
 
+            SavePasswords("md5", passwords, hasher.ComputeMd5Hash);
+            SavePasswords("argon", passwords, hasher.ComputeArgon2IdHash);
         }
 
-        static void SaveArgonPasswords(Hasher hasher, Helpers.PasswordGenerator generator, int amount)
+        static void SavePasswords(string fileName, List<string> passwords, Func<string, string> hashingFunction)
         {
-            var passwords = generator.GeneratePasswords(amount);
-            var hashes = passwords.Select(hasher.ComputeArgon2IdHash).Select(x => new {x.Hash, x.Salt});
-            using var writer = File.CreateText(@"../../../../PasswordGenerator/Output/argon.csv");
+            var hashes = passwords.Select(hashingFunction).Select(x => new {Hash = x});
+            using var writer = File.CreateText($"../../../../PasswordGenerator/Output/{fileName}.csv");
             var csvWriter = new CsvWriter(writer, CultureInfo.CurrentCulture);
-            csvWriter.WriteRecords(hashes);
+            csvWriter.WriteRecords(hashes); 
         }
 
-        static void SaveMd5(Hasher hasher, Helpers.PasswordGenerator generator, int amount)
-        {
-            var passwords = generator.GeneratePasswords(amount);
-            var hashes = passwords.Select(hasher.ComputeMd5Hash).Select(x => new {Hash = x});
-            using var writer = File.CreateText(@"../../../../PasswordGenerator/Output/md5.csv");
-            var csvWriter = new CsvWriter(writer, CultureInfo.CurrentCulture);
-            csvWriter.WriteRecords(hashes);
-        }
-        
     }
         
 }
